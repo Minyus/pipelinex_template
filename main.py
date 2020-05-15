9514b1b6d7166046d97b50b40b2522205f386f71
Minyus <yusuke.minami86@gmail.com>
2020-05-16 00:35:07 +0800

    Show src path

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 import os
 from pathlib import Path
+
 import pipelinex
 from pipelinex import MLflowFlexibleContext
 
+
+print("pipelinex version: ", pipelinex.__version__)
 
 project_path = Path(__file__).resolve().parent
 
@@ -16,7 +19,8 @@ if src_path_str not in sys.path:
 if "PYTHONPATH" not in os.environ:
     os.environ["PYTHONPATH"] = src_path_str
 
-print("pipelinex version: ", pipelinex.__version__)
+print("src path: ", src_path_str)
+
 
 context = MLflowFlexibleContext(project_path)
 context.run()
