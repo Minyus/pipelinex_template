54198aca405a141321243880dc7d61d971ec4a5b
Minyus <yusuke.minami86@gmail.com>
2020-05-16 16:42:38 +0800

    Update for the latest PipelineX

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
-import sys
-import os
-from pathlib import Path
-
-import pipelinex
-from pipelinex import MLflowFlexibleContext
+from pipelinex import __version__, configure_paths, FlexibleContext
 
 
-print("pipelinex version: ", pipelinex.__version__)
+print("pipelinex version: ", __version__)
 
-project_path = Path(__file__).resolve().parent
+project_path, src_path = configure_paths(__file__)
+print("project path: ", project_path)
+print("src path: ", src_path)
 
-src_path = project_path / "src"
-src_path_str = str(src_path)
-
-if src_path_str not in sys.path:
-    sys.path.insert(0, src_path_str)
-
-if "PYTHONPATH" not in os.environ:
-    os.environ["PYTHONPATH"] = src_path_str
-
-print("src path: ", src_path_str)
-
-
-context = MLflowFlexibleContext(project_path)
+context = FlexibleContext(project_path)
 context.run()
